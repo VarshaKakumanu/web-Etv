551edba245ec5fff0ca6906767d6bd846cb186e2
varshakakumanu <varshakakumanu@gmail.com>
2024-07-04 11:39:35 +0530

    updated build errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-const basenameProd = '/react-shadcn-starter'
+const basenameProd = '/react-shadcn-starter/'
 
 export default defineConfig(({ command }) => {
   const isProd = command === 'build'
@@ -18,6 +18,15 @@ export default defineConfig(({ command }) => {
     define: {
       global: {
         basename: isProd ? basenameProd : '',
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+          },
+        },
       },
     },
   }
